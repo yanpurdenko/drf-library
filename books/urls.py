04cfd792aafa294b4cbf08a098088ef225ac89e6
Yan Purdenko <108237132+yanpurdenko@users.noreply.github.com>
2023-04-20 19:09:43 +0200

    added media directory

--- books/urls.py
+++ books/urls.py
@@ -1,6 +1,9 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+from library_service import settings
+
 urlpatterns = [
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- library_service/settings.py
+++ library_service/settings.py
@@ -117,6 +117,10 @@ USE_I18N = True
 
 USE_TZ = True
 
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = BASE_DIR / "media"
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
