9199f258e6e652d6f93e1fb259b829073b932d33
Yan Purdenko <108237132+yanpurdenko@users.noreply.github.com>
2023-04-20 18:03:09 +0200

    fixed ordering Genre model

--- books/models.py
+++ books/models.py
@@ -10,7 +10,7 @@ class Genre(models.Model):
     name = models.CharField(max_length=255)
 
     class Meta:
-        ordering = ["title"]
+        ordering = ["name"]
 
     def __str__(self):
         return self.name
