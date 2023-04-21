19215e57134bf92bb9bfc16a6fd5303c2e536861
Yan Purdenko <108237132+yanpurdenko@users.noreply.github.com>
2023-04-21 18:39:36 +0200

    created IsAdminUserOrReadOnly permission and added permission_classes and authentication_classes to books/views.py

--- books/views.py
+++ books/views.py
@@ -1,14 +1,20 @@
 from rest_framework import viewsets
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from books.models import Genre, Book
+from books.permissions import IsAdminUserOrReadOnly
 from books.serializers import GenreSerializer, BookSerializer
 
 
 class GenreViewSet(viewsets.ModelViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAdminUserOrReadOnly,)
 
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all().prefetch_related("genres")
     serializer_class = BookSerializer
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAdminUserOrReadOnly,)
