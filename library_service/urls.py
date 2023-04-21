d991c8c2594430bddc07c76964c95ab8bb83d4d3
Yan Purdenko <108237132+yanpurdenko@users.noreply.github.com>
2023-04-21 19:56:08 +0200

    borrowings url added to library_service/urls.py

--- library_service/urls.py
+++ library_service/urls.py
@@ -22,6 +22,7 @@ from library_service import settings
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/books/", include("books.urls", namespace="books")),
+    path("api/borrowings/", include("borrowings.urls", namespace="borrowings")),
     path("api/users/", include("users.urls", namespace="users")),
     path("__debug__/", include("debug_toolbar.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
