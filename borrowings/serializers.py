175ccde9d0ee263b322fab5987d53e4e82040d21
Yan Purdenko <108237132+yanpurdenko@users.noreply.github.com>
2023-04-24 19:31:06 +0200

    Made migrations for Borrowing model. In BorrowingSerializer, in create methode added transaction.atomic() and inventory reduction logic

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -1,3 +1,5 @@
+from django.db import transaction
+from django.db.utils import IntegrityError
 from rest_framework import serializers
 
 from borrowings.models import Borrowing
@@ -20,3 +22,11 @@ class BorrowingSerializer(serializers.ModelSerializer):
             "actual_return_date",
             "user",
         )
+
+    def create(self, validated_data):
+        with transaction.atomic():
+            instance = super().create(validated_data)
+            instance.book_id.inventory -= 1
+            instance.book_id.save()
+
+            return instance
