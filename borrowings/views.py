3aeec9d46b599cb8783bd5542aae24e6a4eb8cef
Yan Purdenko <108237132+yanpurdenko@users.noreply.github.com>
2023-04-24 19:59:29 +0200

    filtering for user_id and whether is borrowing still active or not added

--- borrowings/views.py
+++ borrowings/views.py
@@ -12,3 +12,22 @@ class BorrowingsViewSet(
 ):
     queryset = Borrowing.objects.all()
     serializer_class = BorrowingSerializer
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        is_active = self.request.query_params.get("is_active", None)
+
+        if is_active == "1":
+            queryset = queryset.filter(actual_return_date__isnull=True)
+        elif is_active == "0":
+            queryset = queryset.filter(actual_return_date__isnull=False)
+
+        if self.request.user.is_staff:
+            user_id = self.request.query_params.get("user_id", None)
+            if user_id:
+                queryset = queryset.filter(user_id=int(user_id))
+
+                return queryset
+
+        return queryset.filter(user_id=self.request.user)
