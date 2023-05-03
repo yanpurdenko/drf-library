30d9a2f369ed7bb7570b3932073d644aaafc2d10
Yan Purdenko <108237132+yanpurdenko@users.noreply.github.com>
2023-05-03 20:41:22 +0200

    used load_dotenv() instead of simple imports from settings

--- borrowings/service.py
+++ borrowings/service.py
@@ -1,16 +1,19 @@
+import os
+
 import requests
-
+from dotenv import load_dotenv
 from requests import Response
-from library_service.settings import BOT_TOKEN, CHAT_ID
 
-URL = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+load_dotenv()
+
+URL = f"https://api.telegram.org/bot{os.environ.get('BOT_TOKEN')}/sendMessage"
 
 
 def send_telegram_notification(message: str) -> Response:
     return requests.post(
         URL,
         data={
-            "chat_id": CHAT_ID,
+            "chat_id": os.environ.get("CHAT_ID"),
             "text": message,
         },
     )
--- library_service/settings.py
+++ library_service/settings.py
@@ -179,6 +179,3 @@ CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
 CELERY_TASK_SERIALIZER = "json"
 CELERY_RESULT_SERIALIZER = "json"
 CELERY_ACCEPT_CONTENT = ["json"]
-
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-CHAT_ID = os.environ.get("CHAT_ID")
