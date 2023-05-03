67aa059bfba207e9617be07bd6ddf58466a6e7be
Yan Purdenko <108237132+yanpurdenko@users.noreply.github.com>
2023-05-03 20:19:20 +0200

    pagination for books added

--- books/views.py
+++ books/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from books.models import Genre, Book
@@ -13,8 +14,15 @@ class GenreViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAdminUserOrReadOnly,)
 
 
+class BookPagination(PageNumberPagination):
+    page_size = 5
+    page_size_query_param = "page_size"
+    max_page_size = 100
+
+
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all().prefetch_related("genres")
     serializer_class = BookSerializer
+    pagination_class = BookPagination
     authentication_classes = (JWTAuthentication,)
     permission_classes = (IsAdminUserOrReadOnly,)
